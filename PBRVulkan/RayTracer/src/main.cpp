6b2fb6754c7321c1345fc52a241b6787b23be7ce
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2020-10-16 11:11:42 +0200

    Folder renaming

--- PBRVulkan/RayTracer/src/main.cpp
+++ PBRVulkan/RayTracer/src/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	// Compile all shaders from Assets folder
-	std::system("python ./Scripts/Compile.py");
+	std::system("python ./scripts/Compile.py");
 
 	Tracer::Application application;
 
