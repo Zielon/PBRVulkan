59f14636d1a64b66674b5aedf8ba909ffe29cc80
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2021-06-25 19:09:38 +0200

    Windows install fix

--- windows_install.bat
+++ windows_install.bat
@@ -1,22 +1,26 @@
-mkdir Build
-cd Build
-git clone https://github.com/Microsoft/vcpkg.git vcpkg.windows
-cd vcpkg.windows
-git checkout 2021.05.12
-call bootstrap-vcpkg.bat
+IF not EXIST Build (
+	mkdir Build
+	cd Build
+	git clone https://github.com/Microsoft/vcpkg.git vcpkg.windows
+	cd vcpkg.windows
+	git checkout 2021.05.12
+	call bootstrap-vcpkg.bat
 
-vcpkg.exe install ^
-	glfw3:x64-windows ^
-	glm:x64-windows ^
-	imgui:x64-windows ^
-	stb:x64-windows ^
-	tinyobjloader:x64-windows
+	vcpkg.exe install ^
+		glfw3:x64-windows ^
+		glm:x64-windows ^
+		imgui:x64-windows ^
+		stb:x64-windows ^
+		tinyobjloader:x64-windows
 
-vcpkg.exe integrate install
-
-cd ../..
+	vcpkg.exe integrate install
+	cd ../..
+)
 
 cd PBRVulkan/Assets
-git clone https://github.com/Zielon/PBRScenes
+
+IF not EXIST PBRScenes (
+	git clone https://github.com/Zielon/PBRScenes
+)
 
 exit /b
