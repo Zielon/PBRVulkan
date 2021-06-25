be7efb42ccd3cf7c1eaa552bda18cc415ab644ad
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2021-06-25 18:54:46 +0200

    Script update

--- linux_install.sh
+++ linux_install.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 set -e
 
-mkdir Build
-cd Build
-git clone https://github.com/Microsoft/vcpkg.git vcpkg.linux
-cd vcpkg.linux
-git checkout 2021.05.12
-./bootstrap-vcpkg.sh
+if [ ! -d Build ]; then
+  mkdir Build
+  cd Build
+  git clone https://github.com/Microsoft/vcpkg.git vcpkg.linux
+  cd vcpkg.linux
+  git checkout 2021.05.12
+  ./bootstrap-vcpkg.sh
 
-# glfw3:x64-linux requires:
-sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev
+  # glfw3:x64-linux requires:
+  sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev
 
-./vcpkg install \
-	glfw3:x64-linux \
-	glm:x64-linux \
-	imgui:x64-linux \
-	stb:x64-linux \
-	tinyobjloader:x64-linux
+  ./vcpkg install \
+    glfw3:x64-linux \
+    glm:x64-linux \
+    imgui:x64-linux \
+    stb:x64-linux \
+    tinyobjloader:x64-linux
 
-cd ../..
+  cd ../..
+fi
 
 cd PBRVulkan/Assets
-git clone https://github.com/Zielon/PBRScenes
+
+if [ ! -d PBRScenes ]; then
+  git clone https://github.com/Zielon/PBRScenes
+fi
 
 cd ../..
 
