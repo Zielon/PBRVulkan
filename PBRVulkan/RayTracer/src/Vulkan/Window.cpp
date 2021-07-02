7520d6236240ae504c77f58d6e9d2370812f0ae7
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2021-07-02 23:27:22 +0200

    Windows size change

--- PBRVulkan/RayTracer/src/Vulkan/Window.cpp
+++ PBRVulkan/RayTracer/src/Vulkan/Window.cpp
@@ -55,7 +55,7 @@ namespace Vulkan
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-		window = glfwCreateWindow(1200, 1200, "PBRVulkan", nullptr, nullptr);
+		window = glfwCreateWindow(1080, 1080, "PBRVulkan", nullptr, nullptr);
 
 		if (window == nullptr)
 		{
